cf71776503cf21ac71c2b330edb4c6853c01986c
Romain <thiriot2002@gmail.com>
2021-02-26 16:40:16 +0100

    bouton voir demandes
    bouton demandes inscription ( eco+ )

--- resources/views/admin/listeUtilisateur.blade.php
+++ resources/views/admin/listeUtilisateur.blade.php
@@ -1,7 +1,17 @@
 @extends('head.admin')
 @section('content')
-<div style="text-align: center;">
-    <table border="1">
+
+<form action="DemandesInscriptions" align="center" method="post">
+                 
+                  <input type="hidden" name="utilisateur" value={{$utilisateur}}>
+                 
+                      <button type="submit"  class="nav-link">Voir demandes d'inscription</button>
+                 
+              </form>
+<div class="shadow-lg p-3 mb-5 bg-white rounded">
+    <h3 align="center" style="color:#00DFF9";>LISTE DES UTILISATEURS</h3>
+  </div>
+    <table border="2" align="center">
         <thead>
             <tr>
                 <th colspan="2" >Liste des utilisateurs</th>
