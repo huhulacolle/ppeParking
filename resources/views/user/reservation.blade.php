92b5c44b6caa9ac0551b452324def85dc1f82054
Hugo Araujo <araujohugo@msn.com>
2021-02-27 11:24:35 +0100

    Ajout d'un bouton pour faire une reservation

--- resources/views/user/reservation.blade.php
+++ resources/views/user/reservation.blade.php
@@ -5,37 +5,47 @@ Log::debug($today);
 ?>
 @extends('head.user')
 @section('content')
-<center> <h2> Vos RéservationSSSS </h2> </center>
+<center>
+    <h2> Vos Réservations </h2>
+</center>
 <br>
-@if ($dbreserv[0] == 0)
+<form action="Reservation" method="post">
+    <input type="hidden" name="iduser" value={{$info[0]}}>
     <div class="container mb-3 mt-3">
-        <table class="table">
-            <thead>
-                <th scope="col">Réservation N°</th>
-                <th scope="col">File d'attente</th>
-                <th scope="col">Réservation faite le</th>
-                <th scope="col">Date limite</th>
-                <th scope="col">Etat de la reservation</th>
-                <th scope="col"></th>
-            </thead>
-            <tbody>
-                @foreach ($reserv as $reservdata)
-                <?php $annule = 0 ?>
-                    <tr>
-                        <td>
-                            {{$reservdata -> idReservation}}
-                        </td>
-                        <td>
-                            {{$reservdata -> positionFileAttente}}
-                        </td>
-                        <td>
-                            {{$reservdata -> dateDebut}}
-                        </td>
-                        <td>
-                            {{$reservdata -> dateFin}}
-                        </td>
-                        <td>
-                            <?php
+        <p class="text-right">
+            <button type="submit" class="btn btn-link">Faire une réservation</button>
+        </p>
+    </div>
+</form>
+@if ($dbreserv[0] == 0)
+<div class="container mb-3 mt-3">
+    <table class="table">
+        <thead>
+            <th scope="col">Réservation N°</th>
+            <th scope="col">File d'attente</th>
+            <th scope="col">Réservation faite le</th>
+            <th scope="col">Date limite</th>
+            <th scope="col">Etat de la reservation</th>
+            <th scope="col"></th>
+        </thead>
+        <tbody>
+            @foreach ($reserv as $reservdata)
+            <?php $annule = 0 ?>
+            <tr>
+                <td>
+                    {{$reservdata -> idReservation}}
+                </td>
+                <td>
+                    {{$reservdata -> positionFileAttente}}
+                </td>
+                <td>
+                    {{$reservdata -> dateDebut}}
+                </td>
+                <td>
+                    {{$reservdata -> dateFin}}
+                </td>
+                <td>
+                    <?php
                             Log::debug($reservdata -> idReservation);
                             Log::debug($reservdata -> etatReservation);
                             if ($reservdata -> etatReservation == 1) {
@@ -50,25 +60,29 @@ Log::debug($today);
                                 echo 'Validée';
                             }
                             ?>
-                        </td>
-                        <td>
-                            <form action="annuler" method="post">
-                                @csrf
-                                <input type="hidden" name="iduser" value={{$info[0]}}>
-                                <input type="hidden" name="action" value="1">
-                                @if ($annule == 0)
-                                    <button type="submit" value={{$reservdata -> idReservation}} onclick='return confirm("Êtes-vous sûr de vouloir annuler la reservation ?")' name="id" class="btn btn-danger">Annuler</button>
-                                @else
-                                    <button type="submit" value={{$reservdata -> idReservation}} onclick='return confirm("Êtes-vous sûr de vouloir annuler la reservation ?")' name="id" class="btn btn-danger" disabled>Annuler</button>
-                                @endif
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
+                </td>
+                <td>
+                    <form action="annuler" method="post">
+                        @csrf
+                        <input type="hidden" name="iduser" value={{$info[0]}}>
+                        <input type="hidden" name="action" value="1">
+                        @if ($annule == 0)
+                        <button type="submit" value={{$reservdata -> idReservation}}
+                            onclick='return confirm("Êtes-vous sûr de vouloir annuler la reservation ?")' name="id"
+                            class="btn btn-danger">Annuler</button>
+                        @else
+                        <button type="submit" value={{$reservdata -> idReservation}}
+                            onclick='return confirm("Êtes-vous sûr de vouloir annuler la reservation ?")' name="id"
+                            class="btn btn-danger" disabled>Annuler</button>
+                        @endif
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 @else
-    <center> Aucune réservation effectuer </center>
+<center> Aucune réservation effectuer </center>
 @endif
 @endsection
