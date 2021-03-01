5f6b368b9d0683076077ce23a615df312c423aa9
Romain <thiriot2002@gmail.com>
2021-03-01 15:49:51 +0100

    liste utilisateur bootstrap

--- resources/views/admin/listeUtilisateur.blade.php
+++ resources/views/admin/listeUtilisateur.blade.php
@@ -1,33 +1,28 @@
 @extends('head.admin')
 @section('content')
 
-<form action="demandesinscriptions" align="center" method="post">
-@csrf
 
-        <button type="submit"  class="nav-link">Voir demandes d'inscription</button>
-
-              </form>
 <div class="shadow-lg p-3 mb-5 bg-white rounded">
-    <h3 align="center" style="color:#00DFF9";>LISTE DES UTILISATEURS</h3>
-  </div>
-    <table border="2" align="center" width="90%">
-        <thead>
-            <tr>
-                <th colspan="8" >Liste des utilisateurs</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>Id de l'utilisateur   </td>
-                <td>Pseudo de l'utilisateur   </td>
-                <td>Nom de l'utilisateur   </td>
-                <td>Prénom de l'utlisateur   </td>
-                <td>Mail de l'utilisateur  </td>
-                <td>Mot de passe de l'utilisateur   </td>
-                <td>Mot de passe de l'utilisateur oublié   </td>
-                <td></td>
-            </tr>
-            <?php
+    <form action="demandesinscriptions" method="post">
+        @csrf
+
+        <a class="btn btn-info" align="left" href="demandesinscriptions" role="button">Voir demandes d'inscription</a>
+
+
+        <h3 align="center" style="color:#00DFF9" ;>LISTE DES UTILISATEURS</h3>
+    </form>
+</div>
+    <table class="table">
+
+        <th scope="col">Id de l'utilisateur </th>
+        <th scope="col">Pseudo de l'utilisateur </th>
+        <th scope="col">Nom de l'utilisateur </th>
+        <th scope="col">Prénom de l'utlisateur </th>
+        <th scope="col">Mail de l'utilisateur </th>
+        <th scope="col">Mot de passe de l'utilisateur </th>
+        <th scope="col">MDP Oublié? </th>
+        </tr>
+        <?php
                 foreach ($listeUtilisateur as $key => $value) {
                     $idUtilisateur = $value->idUtilisateur;
                     echo '<tr>
@@ -38,14 +33,14 @@
                             <td>'.$value->mail.'</td>
                             <td>'.$value->motDePasseUtilisateur.'</td>
                             <td>'.$value->motDePasseOublie.'</td>
-                            <td><a href="modificationMdpUtilisateur'.$idUtilisateur.'"> Modifier mot de passe</a></td>
+                            <td><a class="btn btn-primary" href="modificationMdpUtilisateur'.$idUtilisateur.'" role="button">Modifier</a></td>
                             <td></td>
                         </tr>';
                 }
             ?>
         </tbody>
     </table>
-    <br><br><br><br><br>
+<br><br><br><br><br>
 <a href="testAdmin">Regarder les places libres</a>
-</div>
+
 @endsection
