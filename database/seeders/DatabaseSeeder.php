a42612d1957a2483ad9507b323cf76f2b47132c9
JulienBois <71647803+JulienBois@users.noreply.github.com>
2021-03-08 12:06:20 +0100

    modification bdd

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,8 +7,7 @@ use App\Models\reservation;
 use App\Models\utilisateur;
 use App\Models\parking;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -63,99 +62,99 @@ class DatabaseSeeder extends Seeder
         ));
         utilisateur::insert(array(
             array(
-                'idUtilisateur' => 1, 'nomUtilisateur' => 'HugoAraujooooo','nom' =>'Hugo', 'Prenom' => 'Araujo','mail'=>'laTourInfernale@hotmail.com', 'motDePasseUtilisateur' => Hash::make('huhuLaColle'), 'estInscrit' => true, 'isAdministrateur' => true ,
+                'idUtilisateur' => 1, 'nomUtilisateur' =>'HugoA','nom' =>'Araujo', 'Prenom' => 'Hugo','mail'=>'araujohugo@msn.com', 'motDePasseUtilisateur' => Hash::make('hugoContreInjections'), 'estInscrit' => true, 'isAdministrateur' => true ,
             ),
             array(
-                'idUtilisateur' => 2, 'nomUtilisateur' => 'RomainTh','nom' =>'Thiriot', 'Prenom' => 'RomainTmtc','mail'=>'ayaRT@hotmail.com', 'motDePasseUtilisateur' => Hash::make('AyaRomain'), 'estInscrit' => true, 'isAdministrateur' => false ,
+                'idUtilisateur' => 2, 'nomUtilisateur' => 'RomainT','nom' =>'Thiriot', 'Prenom' => 'Romain','mail'=>'Romain.thiriot@gmail.com', 'motDePasseUtilisateur' => Hash::make('thiriotMdp'), 'estInscrit' => true, 'isAdministrateur' => false ,
             ),
             array(
-                'idUtilisateur' => 3, 'nomUtilisateur' => 'BoaJulien','nom' =>'Bois', 'Prenom' => 'Julien','mail'=>'julienInconnu@gmail.com', 'motDePasseUtilisateur' => Hash::make('julien1234'), 'estInscrit' => true, 'isAdministrateur' => false ,
+                'idUtilisateur' => 3, 'nomUtilisateur' => 'JulienB','nom' =>'Bois', 'Prenom' => 'Julien','mail'=>'julien.bois.techer@gmail.com', 'motDePasseUtilisateur' => Hash::make('julienBdd'), 'estInscrit' => true, 'isAdministrateur' => false ,
             ),
             array(
-                'idUtilisateur' => 10, 'nomUtilisateur' => 'inconnu','nom' =>'zagzag', 'Prenom' => 'rh','mail'=>'royaumeDesRH@rhworld.com', 'motDePasseUtilisateur' => Hash::make('mdpInconnu') ,'estInscrit' => true,'isAdministrateur' => false ,
+                'idUtilisateur' => 4, 'nomUtilisateur' => 'AlexandreM','nom' =>'Mesle', 'Prenom' => 'Alexandre','mail'=>'moodle.alexandreMesle@gmail.com', 'motDePasseUtilisateur' => Hash::make('haveIBeenPawned'), 'estInscrit' => true, 'isAdministrateur' => false ,
             ),
             array(
-                'idUtilisateur' => 4, 'nomUtilisateur' => 'PasDeReservation','nom' =>'vinci', 'Prenom' => 'guerra','mail'=>'supraluminique@vinci.universe', 'motDePasseUtilisateur' => Hash::make('slatumGreut'),'estInscrit' => false,'isAdministrateur' => false ,
+                'idUtilisateur' => 5, 'nomUtilisateur' => 'GianniB','nom' =>'Bosio', 'Prenom' => 'Gianni','mail'=>'gianni.bosio@hotmail.com', 'motDePasseUtilisateur' => Hash::make('gianniBosioEn'), 'estInscrit' => false, 'isAdministrateur' => false ,
             ),
             array(
-                'idUtilisateur' => 5, 'nomUtilisateur' => 'Escriva','nom' =>'Escriva', 'Prenom' => 'gOublié','mail'=>'escrivaSafari@safari.com', 'motDePasseUtilisateur' => Hash::make('JeKiffeLesParkings') ,'estInscrit' => true,'isAdministrateur' => false ,
+                'idUtilisateur' => 6, 'nomUtilisateur' => 'BenoitL','nom' =>'LaGrangier', 'Prenom' => 'Benoit','mail'=>'Benoit.lagrangier@gmail.com', 'motDePasseUtilisateur' => Hash::make('benoitLagrangier'), 'estInscrit' => true, 'isAdministrateur' => false ,
             ),
             array(
-                'idUtilisateur' => 6, 'nomUtilisateur' => 'bruney','nom' =>'faitesNousDesOrdisQuiMarchent', 'Prenom' => 'DIDIER','mail'=>'jeNeComprendspas@bloodyHell.com', 'motDePasseUtilisateur' => Hash::make('onTheFly'),'estInscrit' => true,'isAdministrateur' => false ,
+                'idUtilisateur' => 7, 'nomUtilisateur' => 'LisaK','nom' => 'Lisa', 'Prenom' => 'klarons','mail'=>'lisaKlarons@gmail.com', 'motDePasseUtilisateur' => Hash::make('lisarvations'), 'estInscrit' => true, 'isAdministrateur' => false ,
             ),
             array(
-                'idUtilisateur' => 7, 'nomUtilisateur' => 'titli','nom' =>'fastFood', 'Prenom' => 'vintage','mail'=>'çaMarcheComment@huawei.com', 'motDePasseUtilisateur' => Hash::make('lesTrains') ,'estInscrit' => true,'isAdministrateur' => false ,
+                'idUtilisateur' => 8, 'nomUtilisateur' => 'JacquesI','nom' =>'Illinois', 'Prenom' => 'Jacques','mail'=>'JacquesIllinois@gmail.com', 'motDePasseUtilisateur' => Hash::make('illinoisUsaJacques'), 'estInscrit' => true, 'isAdministrateur' => false ,
             ),
             array(
-                'idUtilisateur' => 8, 'nomUtilisateur' => 'InèsMGG','nom' =>'Maganga', 'Prenom' => 'Inès','mail'=>'wtfmgg@iSpeakEngish.com', 'motDePasseUtilisateur' => Hash::make('holyShit') ,'estInscrit' => true,'isAdministrateur' => false ,
+                'idUtilisateur' => 9, 'nomUtilisateur' => 'BenjaminP','nom' =>'Pliort', 'Prenom' => 'Benjamin','mail'=>'pliortB@gmail.com', 'motDePasseUtilisateur' => Hash::make('pliortDoitur'), 'estInscrit' => true, 'isAdministrateur' => false ,
             ),
             array(
-                'idUtilisateur' => 9, 'nomUtilisateur' => 'GianniBosioFromCunt','nom' =>'Bosio', 'Prenom' => 'Gianni','mail'=>'gianniBosio@kant.com', 'motDePasseUtilisateur' => Hash::make('narutoXmanonLove') ,'estInscrit' => true,'isAdministrateur' => false ,
+                'idUtilisateur' => 10, 'nomUtilisateur' => 'AlexisM','nom' =>'Momuoir', 'Prenom' => 'Alexis','mail'=>'momuoir@gmail.com', 'motDePasseUtilisateur' => Hash::make('momentdansLeNoir'), 'estInscrit' => true, 'isAdministrateur' => false ,
             ),
             array(
-                'idUtilisateur' => 11, 'nomUtilisateur' => 'MelvRedur','nom' =>'Redureau', 'Prenom' => 'Melvin','mail'=>'MelDokkan@battle.com', 'motDePasseUtilisateur' => Hash::make('CquantiqueToutça') ,'estInscrit' => true,'isAdministrateur' => false ,
+                'idUtilisateur' => 11, 'nomUtilisateur' => 'FabriceF','nom' =>'Flibustier', 'Prenom' => 'Fabrice','mail'=>'fabriceJournal@gmail.com', 'motDePasseUtilisateur' => Hash::make('flibusteurPereEnFils'), 'estInscrit' => true, 'isAdministrateur' => false ,
             ),
             array(
-                'idUtilisateur' => 12, 'nomUtilisateur' => 'Alex','nom' =>'CHERAMNAC', 'Prenom' => 'Alexei','mail'=>'jepechobeaucouptrop@tinder.com', 'motDePasseUtilisateur' => Hash::make('rocketLeague3000€') ,'estInscrit' => false,'isAdministrateur' => false ,
+                'idUtilisateur' => 12, 'nomUtilisateur' => 'KevinV','nom' =>'VonTron', 'Prenom' => 'Kevin','mail'=>'kevinvon@gmail.com', 'motDePasseUtilisateur' => Hash::make('vononmpd'), 'estInscrit' => true, 'isAdministrateur' => false ,
             ),
             array(
-                'idUtilisateur' => 13, 'nomUtilisateur' => 'ProfDeMath','nom' =>'exponentiel', 'Prenom' => 'logarithmique','mail'=>'maths@calculatrice.com', 'motDePasseUtilisateur' => Hash::make('çacemangesansfaim') ,'estInscrit' => true,'isAdministrateur' => false ,
+                'idUtilisateur' => 13, 'nomUtilisateur' => 'AmidouD','nom' =>'Diallo', 'Prenom' => 'Amidou','mail'=>'amidouDiallo@gmail.com', 'motDePasseUtilisateur' => Hash::make('dialphone'), 'estInscrit' => true, 'isAdministrateur' => false ,
             ),
         ));
         reservation::insert(array(
             array(
-                'idReservation' => 2, 'positionFileAttente' => null , 'numeroPlace' => null, 'utilisateur' => 2,'etatReservation' => 0, 'dateDebut' => '2020/08/10', 'dateFin' => '2020/04/17',
+                'idReservation' => 2, 'positionFileAttente' => null , 'numeroPlace' => null, 'utilisateur' => rand(2, 13),'etatReservation' => rand(0,1), 'dateDebut' => '2020/08/10', 'dateFin' => '2020/04/17',
             ),
             array(
-                'idReservation' => 3, 'positionFileAttente' => null , 'numeroPlace' => null, 'utilisateur' => 3 , 'etatReservation' => 0, 'dateDebut' => '2020/01/29', 'dateFin' => '2020/01/20',
+                'idReservation' => 3, 'positionFileAttente' => null , 'numeroPlace' => null, 'utilisateur' => rand(2, 13),'etatReservation' => rand(0,1), 'dateDebut' => '2020/01/29', 'dateFin' => '2020/01/20',
             ),
             array(
-                'idReservation' => 4, 'positionFileAttente' => 1 , 'numeroPlace' => null, 'utilisateur' => 10 ,'etatReservation' => 0, 'dateDebut' => null,'dateFin' => null,
+                'idReservation' => 4, 'positionFileAttente' => 1 , 'numeroPlace' => null, 'utilisateur' => rand(2, 13),'etatReservation' => rand(0,1), 'dateDebut' => null,'dateFin' => null,
             ),
             array(
-                'idReservation' => 5, 'positionFileAttente' => null , 'numeroPlace' => 2, 'utilisateur' => 5 ,'etatReservation' => 0, 'dateDebut' => '2019/01/17','dateFin' => '2019/02/17',
+                'idReservation' => 5, 'positionFileAttente' => null , 'numeroPlace' => 2, 'utilisateur' => rand(2, 13),'etatReservation' => rand(0,1), 'dateDebut' => '2019/01/17','dateFin' => '2019/02/17',
             ),
             array(
-                'idReservation' => 6, 'positionFileAttente' => null , 'numeroPlace' => 7, 'utilisateur' => 5 ,'etatReservation' => 0, 'dateDebut' => '2019/07/10','dateFin' => '2019/08/10',
+                'idReservation' => 6, 'positionFileAttente' => null , 'numeroPlace' => 7, 'utilisateur' => rand(2, 13),'etatReservation' => rand(0,1), 'dateDebut' => '2019/07/10','dateFin' => '2019/08/10',
             ),
             array(
-                'idReservation' => 7, 'positionFileAttente' => null , 'numeroPlace' => 9, 'utilisateur' => 5 ,'etatReservation' => 0, 'dateDebut' => '2019/11/21','dateFin' => '2019/12/21',
+                'idReservation' => 7, 'positionFileAttente' => null , 'numeroPlace' => 9, 'utilisateur' => rand(2, 13),'etatReservation' => rand(0,1), 'dateDebut' => '2019/11/21','dateFin' => '2019/12/21',
             ),
             array(
-                'idReservation' => 8, 'positionFileAttente' => null , 'numeroPlace' => 3, 'utilisateur' => 5 ,'etatReservation' => 0, 'dateDebut' => '2020/01/10','dateFin' => '2019/02/10',
+                'idReservation' => 8, 'positionFileAttente' => null , 'numeroPlace' => 3, 'utilisateur' => rand(2, 13),'etatReservation' => rand(0,1), 'dateDebut' => '2020/01/10','dateFin' => '2019/02/10',
             ),
             array(
-                'idReservation' => 9, 'positionFileAttente' => 3 , 'numeroPlace' => null, 'utilisateur' => 5 ,'etatReservation' => 0, 'dateDebut' => null,'dateFin' => null,
+                'idReservation' => 9, 'positionFileAttente' => 3 , 'numeroPlace' => null, 'utilisateur' => rand(2, 13),'etatReservation' => rand(0,1), 'dateDebut' => null,'dateFin' => null,
             ),
             array(
-                'idReservation' => 10, 'positionFileAttente' => null , 'numeroPlace' => 7, 'utilisateur' => 10 ,'etatReservation' => 1, 'dateDebut' => '2020/01/12','dateFin' => '2020/02/12',
+                'idReservation' => 10, 'positionFileAttente' => null , 'numeroPlace' => 7, 'utilisateur' => rand(2, 13),'etatReservation' => rand(0,1), 'dateDebut' => '2020/01/12','dateFin' => '2020/02/12',
             ),
             array(
-                'idReservation' => 11, 'positionFileAttente' => 4 , 'numeroPlace' => null, 'utilisateur' => 6 ,'etatReservation' => 0, 'dateDebut' => null,'dateFin' => null,
+                'idReservation' => 11, 'positionFileAttente' => 4 , 'numeroPlace' => null, 'utilisateur' => rand(2, 13),'etatReservation' => rand(0,1), 'dateDebut' => null,'dateFin' => null,
             ),
             array(
-                'idReservation' => 12, 'positionFileAttente' => 5 , 'numeroPlace' => null, 'utilisateur' => 7 ,'etatReservation' => 0, 'dateDebut' => null,'dateFin' => null,
+                'idReservation' => 12, 'positionFileAttente' => 5 , 'numeroPlace' => null, 'utilisateur' => rand(2, 13),'etatReservation' => rand(0,1), 'dateDebut' => null,'dateFin' => null,
             ),
             array(
-                'idReservation' => 13, 'positionFileAttente' => 6 , 'numeroPlace' => null, 'utilisateur' => 9 ,'etatReservation' => 0, 'dateDebut' => null,'dateFin' => null,
+                'idReservation' => 13, 'positionFileAttente' => 6 , 'numeroPlace' => null, 'utilisateur' => rand(2, 13),'etatReservation' => rand(0,1), 'dateDebut' => null,'dateFin' => null,
             ),
             array(
-                'idReservation' => 14, 'positionFileAttente' => 7 , 'numeroPlace' => null, 'utilisateur' => 6 ,'etatReservation' => 0, 'dateDebut' => null,'dateFin' => null,
+                'idReservation' => 14, 'positionFileAttente' => 7 , 'numeroPlace' => null, 'utilisateur' => rand(2, 13),'etatReservation' => rand(0,1), 'dateDebut' => null,'dateFin' => null,
             ),
             array(
-                'idReservation' => 15, 'positionFileAttente' => null , 'numeroPlace' => 2, 'utilisateur' => 13 ,'etatReservation' => 0, 'dateDebut' => '2019/01/28','dateFin' => '2019/02/28',
+                'idReservation' => 15, 'positionFileAttente' => null , 'numeroPlace' => 2, 'utilisateur' => rand(2, 13),'etatReservation' => rand(0,1), 'dateDebut' => '2019/01/28','dateFin' => '2019/02/28',
             ),
             array(
-                'idReservation' => 16, 'positionFileAttente' => null , 'numeroPlace' => 7, 'utilisateur' => 13 ,'etatReservation' => 0, 'dateDebut' => '2019/04/27','dateFin' => '2019/05/27',
+                'idReservation' => 16, 'positionFileAttente' => null , 'numeroPlace' => 7, 'utilisateur' => rand(2, 13),'etatReservation' => rand(0,1), 'dateDebut' => '2019/04/27','dateFin' => '2019/05/27',
             ),
             array(
-                'idReservation' => 17, 'positionFileAttente' => null , 'numeroPlace' => 9, 'utilisateur' => 13 ,'etatReservation' => 0, 'dateDebut' => '2019/12/21','dateFin' => '2020/01/21',
+                'idReservation' => 17, 'positionFileAttente' => null , 'numeroPlace' => 9, 'utilisateur' => rand(2, 13),'etatReservation' => rand(0,1), 'dateDebut' => '2019/12/21','dateFin' => '2020/01/21',
             ),
             array(
-                'idReservation' => 18, 'positionFileAttente' => null , 'numeroPlace' => 3, 'utilisateur' => 13,'etatReservation' => 0, 'dateDebut' => '2020/02/04','dateFin' => '2019/03/04',
+                'idReservation' => 18, 'positionFileAttente' => null , 'numeroPlace' => 3, 'utilisateur' => rand(2, 13),'etatReservation' => rand(0,1), 'dateDebut' => '2020/02/04','dateFin' => '2019/03/04',
             ),
             array(
-                'idReservation' => 19, 'positionFileAttente' => 2 , 'numeroPlace' => null, 'utilisateur' => 13,'etatReservation' => 0, 'dateDebut' => null,'dateFin' => null,
+                'idReservation' => 19, 'positionFileAttente' => 2 , 'numeroPlace' => null, 'utilisateur' => rand(2, 13),'etatReservation' => rand(0,1), 'dateDebut' => null,'dateFin' => null,
             ),
         ));
     }
